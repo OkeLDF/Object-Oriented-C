047237d44bf77623b610a6c5ecbc864e7ba4a3b6
OkeLDF <otaviofolador43@gmail.com>
2024-12-07 22:59:03 -0300

    Testing Override

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include "Circle.h"
 
 int main(){
-    Circle *circle = newCircle(0.5);
+    Circle *circle = newCircle(1.0);
 
     double c = circle->getCircumference(circle);
+    double p = circle->obj->getPerimeter(circle->obj);
+    double a = circle->obj->getArea(circle->obj);
+    
     circle->obj->print(circle->obj);
 
-    printf("Circumference: %f", c);
+    printf("Circumference: %f\n", c);
+    printf("Perimeter: %f\n", p);
+    printf("Area: %f\n", a);
 
     return 0;
 }
